50e904b922e8ffef7c5dcdb938e2d6cdff507314
Temesgen Gebremariam <110997234+tmsgnka@users.noreply.github.com>
2022-12-08 00:43:00 +0300

    tasks

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,29 +1,16 @@
 #!/usr/bin/python3
-
 def to_subtract(list_num):
-
     to_sub = 0
-
     max_list = max(list_num)
 
-
-
     for n in list_num:
-
         if max_list > n:
-
             to_sub += n
-
-
 
     return (max_list - to_sub)
 
 
-
-
-
 def roman_to_int(roman_string):
-
     if not roman_string:
         return 0
 
